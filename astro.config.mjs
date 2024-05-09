bf14da6d39f6694b18a80cd8b79f57fac71de5b5
shifat514 <shifat514@gmail.com>
2024-05-09 17:14:59 +0600

    install tailwind and change mode to hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
-
 import vue from "@astrojs/vue";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "hybrid",
   adapter: cloudflare(),
-  integrations: [vue()]
+  integrations: [vue(), tailwind()]
 });
